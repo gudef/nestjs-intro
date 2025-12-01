5fe4839b749a8b1dc72fabc1707b36db31dad9ec
Sarat Pintharnthong <saratpintharnthong@MacBookAirSarat.local>
2025-12-01 13:34:17 +0700

    Deployment

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -11,4 +11,7 @@ export const typeOrmConfig = registerAs(
         password: process.env.DB_PASSWORD,
         database: process.env.DB_DATABASE,
         synchronize: Boolean(process.env.DB_SYNC ?? false),
+        ssl: {
+            rejectUnauthorized: false,
+        }
     }))
